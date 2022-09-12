2ed7c0c1ff14ac1d1b4d1846ea7abef5580ea7dc
Chengtao <58400041+TaoLyn838@users.noreply.github.com>
2022-09-12 13:20:12 -0400

    Lab1 done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,14 +12,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
+    @IBOutlet weak var choice_a: UIButton!
+    @IBOutlet weak var choice_b: UIButton!
+    @IBOutlet weak var choice_c: UIButton!
+    let DefaultBackgroundCol = UIColor.white
+    let DefaultTextCol = UIColor.black
+    
+    var toggle = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Rounded Button
+        choice_a.layer.cornerRadius = 20
+        choice_a.layer.masksToBounds = true
+        choice_b.layer.cornerRadius = 20
+        choice_b.layer.masksToBounds = true
+        choice_c.layer.cornerRadius = 20
+        choice_c.layer.masksToBounds = true
+        
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if backLabel.backgroundColor != DefaultBackgroundCol {
+            backLabel.backgroundColor = DefaultBackgroundCol
+        }
+        if backLabel.textColor != DefaultTextCol {
+            backLabel.textColor = DefaultTextCol
+        }
+        frontLabel.isHidden = toggle;
+        toggle = !toggle
     }
     
+    @IBAction func TapChoiceButton(_ sender: UIButton) {
+        if backLabel.tag == sender.tag {
+            frontLabel.isHidden = true
+            sender.backgroundColor = UIColor.systemTeal
+            backLabel.backgroundColor = UIColor.systemTeal
+            backLabel.textColor = UIColor.white
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,21 @@
+## FLashcards
+
+## Lab 1
+
+### App Description
+A simple flashcards App used for studing <strong>Hiragana</strong> and <strong>Katakana</strong>. 
+<br/>Users can either tap the flashcard to see the answer to a question or tap the choice button for a self-test. 
+
+### App Walk-though
+
+<img src= "https://github.com/TaoLyn838/codepath-prework/blob/main/prework/App%20walk-though.gif" width="400" height="800"> <br>
+## Required
+- [x] Create New Project in Xcode
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHub
+## Optional
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
